3d89fa48b998ff7c59f157dff40806138d5f9cc0
47cult <soso5goga@gmail.com>
2023-10-27 22:53:05 +0300

    lesson19

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
+
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import FullPizza from './pages/FullPizza';
 import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
+import MainLayout from './layouts/MainLayout';
 
 
 
@@ -17,24 +19,27 @@ function App() {
 
   
   return (
-    <div className="wrapper">
+
 
 
 
 
 
       
-      <Header />
-      <div className="content">
-        <div className="container"></div>
+
+     
+
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={<MainLayout />}>
+          <Route path="" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="pizza/:id" element={<FullPizza />} />
           <Route path="*" element={<NotFound />} />
+          </Route>
         </Routes>
-      </div>
+
       
-    </div>
+
   );
 }
 
